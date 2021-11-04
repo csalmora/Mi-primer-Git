4b17a1cb87b8b89ca9c6f151210302b09d5f2785
Christian Salmoral gonzalez <csalmora@car6s1.42barcelona.com>
2021-11-04 21:45:48 +0100

    CommitActualizaciones

--- C/ex05/ft_print_comb.c
+++ C/ex05/ft_print_comb.c
@@ -1,10 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csalmora <csalmora@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/04 21:12:18 by csalmora          #+#    #+#             */
+/*   Updated: 2021/11/04 21:12:42 by csalmora         ###   ########.bcn      */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void ft_print_comb.c (int numero){
+void ft_print_comb (){
 
-	//condiciones son if((a!=b)&&(a!=c)&&(b!=c)){
-	//write(1,&a,1)
-	//write(1,&b,1)
-	//write(1,&c,1)
-	//write(1,&" ,",1)
-	//
+char a;
+char b;
+char c;
+ a='0';
+	
+ while(a<='7'){
+ b = a+1;
+	while (b<='8'){
+	c = b+1;
+	  while (c<='9'){
+	  write(1,&a, 1);
+	  write(1,&b,1);
+	  write(1,&c,1);
+	  write(1,", ",2);
+
+	  c++;
+		            }
+	  b++;
+				  }
+	  a++;
+			  }
+	}
